49d79dbd56945c7dfc027ec16c0d90e843149373
Durga bhavani <113273276+Durgabhavani379@users.noreply.github.com>
2024-06-14 20:50:40 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Interval from './Interval';
+import Components from './Components';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+ <Interval/>,
+  <App />,
+  <Components/>
+  <Components/>
+  </React.StrictMode>
+);
+
+
+
